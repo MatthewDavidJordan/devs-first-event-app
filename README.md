15f46fab67479d9106d725d842c003e9d7077200
Matthew Jordan <mdj82@georgetown.edu>
2024-09-05 09:42:49 -0400

    Trying CORS again through next config

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Hoya Developers First Event App
+
+## Front-end
+The front-end scoreboard to show the count of emails.
+
+## Back-end
+The back-end of this app is a couple API endpoints. 
+This app also connects to a Supabase database. To update the scoreboard 
+
+### API Routes
+The main endpoint can be found at /api/bootcamp/submitNedId
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    
+    // Add CORS headers to all responses
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'POST,GET,OPTIONS',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'Content-Type',
+                    },
+                ],
+            },
+        ];
+    }
+};
 
 export default nextConfig;
