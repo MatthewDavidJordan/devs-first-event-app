3dfe454586e389ac995d24fdba5f851e8b22ac1e
SnugTop <camjdaly@gmail.com>
2024-09-08 03:33:47 -0400

    Cleaned up code. Still need to go through and add comments for better readability

--- src/models.ts
+++ src/models.ts
@@ -1,7 +1,6 @@
 //models.ts
 import { Tables } from "@/lib/supabase";
 
-// base types
 export type Emails = Tables<"emails"> & {
   nonce: string; // New field for verification nonce
   confirmed: boolean; // New field to track if the email is verified
@@ -9,7 +8,6 @@ export type Emails = Tables<"emails"> & {
 
 export type Teams = Tables<"teams">;
 
-// abstract types
 export type ScoreData = {
   email_count: number;
 };
